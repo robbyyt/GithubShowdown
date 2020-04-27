e795504a09b879eb04966dd4fd02895f960002f6
robbyyt <49028058+robbyyt@users.noreply.github.com>
2020-04-27 22:52:23 +0300

    added main-page component and changed some styling

--- angular-github-showdown/src/app/core/core.module.ts
+++ angular-github-showdown/src/app/core/core.module.ts
@@ -5,12 +5,14 @@ import { SharedModule } from '../shared/shared.module';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { MainContainerComponent } from './pages/landing-page/components';
 import { MaterialModule } from '../material/material.module';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 
 @NgModule({
   declarations: [
     LandingPageComponent,
-    MainContainerComponent
+    MainContainerComponent,
+    MainPageComponent
   ],
   imports: [
     CommonModule,
